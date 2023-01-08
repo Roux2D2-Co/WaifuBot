91a98407a491597fcff71379a075632d0028e920
Le-Roux-Nard <guymaxyt@gmail.com>
2023-01-08 23:48:17 +0100

    hotfix

--- src/interactions/Commands/ChatInputCommands/import.ts
+++ src/interactions/Commands/ChatInputCommands/import.ts
@@ -10,26 +10,24 @@ export default {
 	guilds: ["780715935593005088"],
 
 	async execute(interaction: ChatInputCommandInteraction): Promise<void> {
-		await interaction.reply("Nique, faut que je puisse travailler trkl");
-		return;
-		// await interaction.deferReply({ ephemeral: true });
-		// const userId = interaction.user.id;
-		// let userDatabaseProfile = await UserModel.findOne({ userId });
-		// if (!!userDatabaseProfile) {
-		// 	await interaction.editReply("Votre profil a déjà été importé.");
-		// 	return;
-		// } else {
-		// 	const { data: userProfile, status } = await axios.get(`${WAIFU_API_URL}${userId}`, { transformResponse: (data) => JSON.parse(data) });
-		// 	if (status !== 200) {
-		// 		await interaction.editReply("Une erreur est survenue lors de la récupération de votre profil.");
-		// 		return;
-		// 	} else {
-		// 		console.log(userProfile);
-		// 		userDatabaseProfile = new UserModel(userProfile);
-		// 		await userDatabaseProfile.save();
-		// 		await interaction.editReply("Votre profil a bien été importé.");
-		// 	}
-		// }
+		await interaction.deferReply({ ephemeral: true });
+		const userId = interaction.user.id;
+		let userDatabaseProfile = await UserModel.findOne({ id: userId });
+		if (!!userDatabaseProfile) {
+			await interaction.editReply("Votre profil a déjà été importé.");
+			return;
+		} else {
+			const { data: userProfile, status } = await axios.get(`${WAIFU_API_URL}${userId}`, { transformResponse: (data) => JSON.parse(data) });
+			if (status !== 200) {
+				await interaction.editReply("Une erreur est survenue lors de la récupération de votre profil.");
+				return;
+			} else {
+				console.log(userProfile);
+				userDatabaseProfile = new UserModel(userProfile);
+				await userDatabaseProfile.save();
+				await interaction.editReply("Votre profil a bien été importé.");
+			}
+		}
 	},
 	type: ApplicationCommandType.ChatInput,
 } as ChatInputApplicationCommandData;
