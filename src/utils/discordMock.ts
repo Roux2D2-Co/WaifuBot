9afc045dfabc5e29bcbc6c1d7c6157e76e9a4623
Le-Roux-Nard <guymaxyt@gmail.com>
2024-01-08 18:54:41 +0100

    update packages + fix type related errors

--- src/utils/discordMock.ts
+++ src/utils/discordMock.ts
@@ -1,13 +1,14 @@
-import { ChatInputCommandInteraction, Client, APIInteraction, CacheType, CommandInteractionOption } from "discord.js";
+import { ChatInputCommandInteraction, Client, APIInteraction, CacheType, CommandInteractionOption, DiscordjsError } from "discord.js";
 
 export class MockedInteraction extends ChatInputCommandInteraction {
 	constructor(client: Client, interaction: APIInteraction) {
+		if (!client.isReady()) throw new DiscordjsError("Client is not ready");
 		super(client, interaction);
 	}
 
 	static formatInputDataFromParsedInteraction(parsedInteraction: ChatInputCommandInteraction, options: CommandInteractionOption<CacheType>) {
 		const interactionJson = parsedInteraction.toJSON() as { [key: string]: any };
 		interactionJson.options = options;
-    return interactionJson;
+		return interactionJson;
 	}
 }
